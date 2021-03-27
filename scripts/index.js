09210e354d3b7e635cf682db2553a4847a6eda60
Ruslan Murzin <rus-lan.murzin@yandex.ru>
2021-03-28 00:43:09 +0300

    feat: отрисовать фотографии из начального массива initialCards

--- scripts/index.js
+++ scripts/index.js
@@ -1,29 +1,83 @@
+// временно возьмем начальный массив фотографий из 5-й работы
+const initialCards = [
+  {
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
+];
+
+// находим элемент фотогалереи из тэга template
+const photoCardTemplate = document.querySelector('.template-photo-card').content.querySelector('.photo-gallery__item');
+console.log(photoCardTemplate);
+
+// находим пустую галерею фотографий
+const photoGallery = document.querySelector('.photo-gallery__list');
+
+/* для каждого элемента начального массива фотографий initialCards
+* отрисуем фотографию в пустом на данный момент
+* блоке photo-gallery__list
+*/
+initialCards.forEach(function(initialCard) {
+  // клонируем элемент фотогалереи для каждой фото-карточки
+  const photoGalleryItem = photoCardTemplate.cloneNode(true);
+
+  /** присваиваем элементам фото-карточки необходимые значения
+    * из соответствующих значений ключей объектов
+    * массива начальных фотографий initialCards
+  */
+  photoGalleryItem.querySelector('.photo-card__title').textContent = initialCard.name;
+  photoGalleryItem.querySelector('.photo-card__img').src = initialCard.link;
+
+  // отрисовка каждой фото-карточки в конце фотогалереи
+  photoGallery.append(photoGalleryItem);
+});
+
 //находим popup с формой
-let popup = document.querySelector('.popup');
+const popup = document.querySelector('.popup');
 
 // находим форму в popup
-let form = popup.querySelector('.edit-form')
+const form = popup.querySelector('.edit-form')
 
 // находим инпут с именем
-let nameInput = form.querySelector('[name=profile-name]');
+const nameInput = form.querySelector('[name=profile-name]');
 
 // находим инпут с описанием
-let descriptionInput = form.querySelector('[name=profile-description]');
+const descriptionInput = form.querySelector('[name=profile-description]');
 
 // находим кнопку редактировать профиль, она же открывает popup форму
-let openPopupButton = document.querySelector('.profile__edit-button');
+const openPopupButton = document.querySelector('.profile__edit-button');
 
 // находим имя профиля
-let profileName = document.querySelector('.profile__name');
+const profileName = document.querySelector('.profile__name');
 
 // находим описание профиля
-let profileDescription = document.querySelector('.profile__description');
+const profileDescription = document.querySelector('.profile__description');
 
 // находим кнопку закрыть popup с формой
-let closePopupButton = popup.querySelector('.popup__close-button');
+const closePopupButton = popup.querySelector('.popup__close-button');
 
 // метод открытия popup-а путем добавления класса
-let openPopup = function () {
+const openPopup = function () {
   // так же совместим открытие попапа с добавлением информации в input-ы формы
   // для улучшения читабельности кода изменили setAttribute на свойство value
   nameInput.value = profileName.textContent;
@@ -33,14 +87,14 @@ let openPopup = function () {
 }
 
 // метод закрытия popup-а путем удаления класса
-let closePopup = function () {
+const closePopup = function () {
   popup.classList.remove('popup_opened');
   // удалили из функции "лишний" код, т.к. при закрытии данные никуда копироваться не должны
 }
 
 // метод для сохранения введенной информации в форме (с отменой стандартного поведения - отправки формы)
 // с последующим закрытием формы
-let saveChanges = function (event) {
+const saveChanges = function (event) {
   event.preventDefault();
   profileName.textContent = nameInput.value;
   profileDescription.textContent = descriptionInput.value;
